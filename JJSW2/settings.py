ce96c12c336f4e7a15c5983519e581e88e1559dc
Juanda099999999 <jdhuertas10@misena.edu.co>
2022-09-28 10:20:43 -0500

    Update settings.py

--- JJSW2/settings.py
+++ JJSW2/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j)si2uug&(-!-p%7u_#^r*4a2+kd(w$mz2ydy(^^iwd#nt5%sx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://*.jjswkairos.herokuapp.com']
