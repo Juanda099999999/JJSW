2539cd1efd56d6acf225cf62730b5147d18eed62
Juanda099999999 <jdhuertas10@misena.edu.co>
2022-09-29 11:57:52 -0500

    Update settings.py

--- JJSW2/settings.py
+++ JJSW2/settings.py
@@ -330,7 +330,7 @@ STATIC_DIRS = (
 STATIC_TMP = os.path.join(BASE_DIR,"static")
 STATIC_ROOT = os.path.join(BASE_DIR,"static")
 os.makedirs(STATIC_ROOT, exist_ok=True)
-MEDIA_URL = 'media/'
+MEDIA_URL = 'media'
 MEDIAFILES_DIRS = (
     
     os.path.join(BASE_DIR,"media"),
